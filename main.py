2a773f808512f0324f6efeb4153b8c8f0f500e50
Jomari Francisco <JFrancisco@cpsbc.ca>
2023-11-20 13:46:46 -0800

    implement threading and quiting the function

--- main.py
+++ main.py
@@ -12,28 +12,28 @@ class Timer:
     def __init__(self):
         self.counter = 0
         self.startClock = True
+        self.getUserInputBool = True
     
     def clock(self):
-        while self.startClock:
-            self.secondsCounter = 1
-            while self.secondsCounter % 50 is not 0:
-                time.sleep(0.1)
-                self.secondsCounter += 1
-            self.counter += 1 
+        while self.startClock: # Because it is like this it will always have to wait 1 second before it can stop the thread since it is asleep in the while function
+            time.sleep(1)
+            self.counter += 1
             print(self.counter)
 
-def getUserInput():
-    userInput = int(input('Enter -1 to Quit\n'))
-    if userInput == -1:
-        timer.startClock = False
-
+    def getUserInput(self):
+        while self.getUserInputBool:
+            self.userInput = (input('Enter q to Quit\n'))
+            if self.userInput == 'q':
+                self.startClock = False
+                self.getUserInputBool = False
 
 
 if __name__ == "__main__":
 
     timer = Timer()
     t1 = threading.Thread(target = timer.clock)
+    t2 = threading.Thread(target = timer.getUserInput)
+    
     t1.start()
-
-    t2 = threading.Thread(target = getUserInput)
     t2.start()
+    
